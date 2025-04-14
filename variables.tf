4fc65a487a678587db984a56d834173e51eb7235
Aatreyee Mukherjee <aatreyee.mukherjee@ibm.com>
2025-04-14 10:08:37 +0530

    initial commit

--- variables.tf
+++ variables.tf
@@ -53,6 +53,22 @@ variable "trusted_profile_policies" {
     pattern       = optional(string)
   }))
   description = "A list of Trusted Profile Policy objects that are applied to the Trusted Profile created by the module."
+
+
+  validation {
+    condition = alltrue([
+      for i, policy in var.trusted_profile_policies : (
+        length(compact([
+          (lookup(policy, "account_management", null) != null ? "account_management" : null),
+          (lookup(policy, "resources", null) != null ? "resources" : null),
+          (lookup(policy, "resource_attributes", null) != null ? "resource_attributes" : null)
+        ])) == 1
+      )
+    ])
+    error_message = "Each trusted_profile_policy must have exactly one of `account_management`, `resources`, or `resource_attributes` set and non-null. These are mutually exclusive."
+  }
+
+
 }
 
 variable "trusted_profile_claim_rules" {
@@ -75,7 +91,74 @@ variable "trusted_profile_claim_rules" {
 
   description = "A list of Trusted Profile Claim Rule objects that are applied to the Trusted Profile created by the module."
 
-  default = null
+  default = []
+
+  validation {
+    condition = var.trusted_profile_claim_rules == null ? true : (
+      length([
+        for i, claim in var.trusted_profile_claim_rules : (
+          contains(["Profile-SAML", "Profile-CR"], claim.type)
+        )
+      ]) == length(var.trusted_profile_claim_rules)
+    )
+    error_message = "Each value in `var.trusted_profile_claim_rules[*].type` must be either `Profile-SAML` or `Profile-CR`."
+  }
+
+  validation {
+    condition = (
+      alltrue(flatten([
+        for claim in var.trusted_profile_claim_rules : [
+          for condition in claim.conditions :
+          contains(["EQUALS", "NOT_EQUALS", "EQUALS_IGNORE_CASE", "NOT_EQUALS_IGNORE_CASE", "CONTAINS", "IN"], condition.operator)
+        ]
+      ]))
+    )
+    error_message = "Each item in `var.trusted_profile_claim_rules.conditions.operator` must be one of the following: `EQUALS`, `NOT_EQUALS`, `EQUALS_IGNORE_CASE`, `NOT_EQUALS_IGNORE_CASE`, `CONTAINS`, `IN`."
+  }
+
+  validation {
+    condition = (
+      length([
+        for claim in var.trusted_profile_claim_rules :
+        !contains(keys(claim), "cr_type") || claim.type == "Profile-CR"
+      ]) == length(var.trusted_profile_claim_rules)
+    )
+    error_message = "If `cr_type` is provided in a claim, then type must be `Profile-CR`."
+  }
+
+
+  validation {
+    condition = (
+      length([
+        for claim in var.trusted_profile_claim_rules :
+        !contains(keys(claim), "cr_type") || contains(["VSI", "IKS_SA", "ROKS_SA"], claim.cr_type)
+      ]) == length(var.trusted_profile_claim_rules)
+    )
+    error_message = "Value for `cr_type` must be one of the following: `VSI`, `IKS_SA`, `ROKS_SA`."
+  }
+
+
+  validation {
+    condition = (
+      length([
+        for claim in var.trusted_profile_claim_rules :
+        !contains(keys(claim), "expiration") || claim.type == "Profile-SAML"
+      ]) == length(var.trusted_profile_claim_rules)
+    )
+    error_message = "If `expiration` is provided in a claim, then type must be `Profile-SAML`."
+  }
+
+
+  validation {
+    condition = (
+      length([
+        for claim in var.trusted_profile_claim_rules :
+        !contains(keys(claim), "realm_name") || claim.type == "Profile-SAML"
+      ]) == length(var.trusted_profile_claim_rules)
+    )
+    error_message = "If `realm_name` is provided in a claim, then type must be `Profile-SAML`."
+  }
+
 }
 
 variable "trusted_profile_links" {
@@ -94,5 +177,47 @@ variable "trusted_profile_links" {
 
   description = "A list of Trusted Profile Link objects that are applied to the Trusted Profile created by the module."
 
-  default = null
+  default = []
+
+
+
+  validation {
+    condition = (
+      var.trusted_profile_links == null || length([
+        for link in var.trusted_profile_links :
+        contains(["VSI", "IKS_SA", "ROKS_SA"], link.cr_type)
+      ]) == length(var.trusted_profile_links)
+    )
+    error_message = "Each `cr_type` in `trusted_profile_links` must be one of the following: `VSI`, `IKS_SA`, `ROKS_SA`."
+  }
+
+  validation {
+    condition = (
+      var.trusted_profile_links == null || length(flatten([
+        for i, link in var.trusted_profile_links : [
+          for j, obj in link.links :
+          (lookup(obj, "namespace", null) == null && link.cr_type == "VSI") || link.cr_type == "ROKS_SA" || link.cr_type == "IKS_SA"
+        ]
+        ])) == length(flatten([
+        for link in var.trusted_profile_links : link.links
+      ]))
+    )
+    error_message = "A `namespace` in `links` should only be provided if `cr_type` is `IKS_SA` or `ROKS_SA`."
+  }
+
+  validation {
+    condition = (
+      var.trusted_profile_links == null || length(flatten([
+        for i, link in var.trusted_profile_links : [
+          for j, obj in link.links :
+          (lookup(obj, "name", null) == null && link.cr_type == "VSI") || link.cr_type == "ROKS_SA" || link.cr_type == "IKS_SA"
+        ]
+        ])) == length(flatten([
+        for link in var.trusted_profile_links : link.links
+      ]))
+    )
+    error_message = "A `name` in `links` should only be provided if `cr_type` is `IKS_SA` or `ROKS_SA`."
+  }
+
+
 }
