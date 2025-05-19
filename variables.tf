07e3a8053e14397731d17b4f377024a0e1a37617
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2025-05-19 11:33:24 +0100

    fix: added a fix to prevent resources getting destroyed when a trusted profile name is being renamed<br>- `trusted_profile_policies` now has a required attribute called `unique_identifier` which must have a unique value per policy. It is only used by terraform for building map objects, it is not use for any actual resource naming. Changing this value will cause resources to be recreated.<br>- `trusted_profile_claim_rules` now has a required attribute called `unique_identifier` which must have a unique value per claim rule. It is only used by terraform for building map objects, it is not use for any actual resource naming. Changing this value will cause resources to be recreated.<br>- `trusted_profile_links` ` now has a required attribute called `unique_identifier` which must have a unique value per profile link. It is only used by terraform for building map objects, it is not use for any actual resource naming. Changing this value will cause resources to be recreated. (#181)
    BREAKING CHANGE: If upgrading from a previous version, you can prevent resources from being recreated if you use the correct value for the new `unique_identifier` attribute. For example, if you used the following value for trusted profile name: `trusted_profile_name = "my-profile"` you should use the following naming conventions:
    ### Trusted profile policies
    ```
    trusted_profile_policies = [
        {
          unique_identifier = "my-profile-0" # use the format of "<trusted profile name>-0" for first item in the list
          ...
          ...
        },
        {
          unique_identifier = "my-profile-1" # use the format of "<trusted profile name>-1" for second item in the list, and so on
          ...
          ...
        }
    ```
    ### Trusted profile policies
    ```
    trusted_profile_claim_rules = [
        {
          unique_identifier = "my-profile-0" # use the format of "<trusted profile name>-0" for first item in the list
          ...
          ...
        },
        {
          unique_identifier = "my-profile-1" # use the format of "<trusted profile name>-1" for second item in the list, and so on
          ...
          ...
        }
    ```
    ### Trusted profile links
    ```
    trusted_profile_links = [
        {
          unique_identifier = "my-profile-0" # use the format of "<trusted profile name>-0" for first item in the list
          ...
          ...
        },
        {
          unique_identifier = "my-profile-1" # use the format of "<trusted profile name>-1" for second item in the list, and so on
          ...
          ...
        }
    ```

--- variables.tf
+++ variables.tf
@@ -44,6 +44,7 @@ variable "trusted_profile_identity" {
 
 variable "trusted_profile_policies" {
   type = list(object({
+    unique_identifier  = string
     roles              = list(string)
     account_management = optional(bool)
     description        = optional(string)
@@ -81,7 +82,7 @@ variable "trusted_profile_policies" {
     rule_operator = optional(string)
     pattern       = optional(string)
   }))
-  description = "A list of Trusted Profile Policy objects that are applied to the Trusted Profile created by the module."
+  description = "A list of Trusted Profile Policy objects that are applied to the Trusted Profile created by the module. NOTE: The `unique_identifier` attributes are only used by terraform for building map objects, it is not use for any actual resource naming. Changing this value will cause resources to be recreated."
 
   validation {
     condition = alltrue([
@@ -95,11 +96,17 @@ variable "trusted_profile_policies" {
     ])
     error_message = "Each trusted_profile_policy must have exactly one of `account_management`, `resources`, or `resource_attributes` set and non-null. These are mutually exclusive."
   }
+
+  validation {
+    condition     = length(var.trusted_profile_policies[*].unique_identifier) == length(distinct(var.trusted_profile_policies[*].unique_identifier))
+    error_message = "Each `unique_identifier` must be unique in `trusted_profile_policies`."
+  }
 }
 
 variable "trusted_profile_claim_rules" {
   type = list(object({
     # required arguments
+    unique_identifier = string
     conditions = list(object({
       claim    = string
       operator = string
@@ -188,12 +195,18 @@ variable "trusted_profile_claim_rules" {
     )
     error_message = "If `realm_name` is provided, then `type` must be `Profile-SAML`."
   }
+
+  validation {
+    condition     = length(var.trusted_profile_claim_rules[*].unique_identifier) == length(distinct(var.trusted_profile_claim_rules[*].unique_identifier))
+    error_message = "Each 'unique_identifier' must be unique in 'trusted_profile_claim_rules'."
+  }
 }
 
 variable "trusted_profile_links" {
   type = list(object({
     # required arguments
-    cr_type = string
+    unique_identifier = string
+    cr_type           = string
     links = list(object({
       crn       = string
       namespace = optional(string)
@@ -231,5 +244,10 @@ variable "trusted_profile_links" {
       ]))
     )
     error_message = "A `namespace` in `links` should only be provided if `cr_type` is `IKS_SA` or `ROKS_SA`."
+  }
+
+  validation {
+    condition     = length(var.trusted_profile_links[*].unique_identifier) == length(distinct(var.trusted_profile_links[*].unique_identifier))
+    error_message = "Each 'unique_identifier' must be unique in 'trusted_profile_links'."
   }
 }
