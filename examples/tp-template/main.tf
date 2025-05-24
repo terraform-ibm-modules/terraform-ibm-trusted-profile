60b3b5826fadaf414bbb7318f022735a8e35a187
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-05-24 15:24:32 +0100

    chore(deps): update terraform terraform-ibm-modules/cos/ibm to v9 (#185)

--- examples/tp-template/main.tf
+++ examples/tp-template/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "cos" {
   source            = "terraform-ibm-modules/cos/ibm"
-  version           = "8.21.25"
+  version           = "9.0.3"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${var.prefix}-cos"
   cos_tags          = var.resource_tags
