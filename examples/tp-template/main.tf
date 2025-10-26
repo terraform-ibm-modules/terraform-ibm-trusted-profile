7cb9a0c0e7405052e120165256d87b6dbfe193f4
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-10-26 06:23:49 +0000

    chore(deps): update terraform terraform-ibm-modules/cos/ibm to v10.5.2

--- examples/tp-template/main.tf
+++ examples/tp-template/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "cos" {
   source            = "terraform-ibm-modules/cos/ibm"
-  version           = "10.5.1"
+  version           = "10.5.2"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${var.prefix}-cos"
   cos_tags          = var.resource_tags
