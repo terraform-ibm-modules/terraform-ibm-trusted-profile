18d09ce162f4cc118c32d5cf8210ba44dd1ffa16
Jordan-Williams2 <Jordan.Williams2@ibm.com>
2026-02-03 21:43:06 +0000

    chore: remove test files

--- tests/other_test.go
+++ tests/other_test.go
@@ -2,10 +2,8 @@
 package test
 
 import (
-	"strings"
 	"testing"
 
-	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
 )
@@ -68,69 +66,6 @@ func TestRunTemplateUpgrade(t *testing.T) {
 	output, err := options.RunTestUpgrade()
 	if !options.UpgradeTestSkipped {
 		assert.Nil(t, err, "This should not have errored")
-		assert.NotNil(t, output, "Expected some output")
-	}
-}
-
-// TestModuleWithDynamicAccounts tests the trusted-profile-template module
-// with dynamic account IDs to validate for_each dependency fixes
-func TestModuleWithDynamicAccounts(t *testing.T) {
-	t.Parallel()
-
-	options := &terraform.Options{
-		TerraformDir: "./module-with-dynamic-accounts",
-		NoColor:      true,
-		// Reduce verbosity - only output errors
-		Vars: map[string]interface{}{},
-	}
-
-	_, err := terraform.InitE(t, options)
-	if err != nil {
-		t.Fatalf("Init failed: %v", err)
-	}
-
-	// Test the actual module with dynamic account IDs
-	_, err = terraform.PlanE(t, options)
-
-	if err != nil {
-		errorStr := err.Error()
-		if strings.Contains(errorStr, "not a part of any enterprise") {
-			t.Skip("Skipping test - requires enterprise account")
-			return
-		}
-		if strings.Contains(errorStr, "Invalid for_each argument") {
-			t.Logf("Dependency issue detected in module")
-		} else {
-			t.Logf("Plan failed for other reasons")
-		}
-	} else {
-		t.Logf("Plan succeeded - module working correctly")
-	}
-}
-
-// TestKeyStabilityUpgrade tests that our fix for resource key stability works correctly
-// This test validates that reordering input lists doesn't cause resource recreation
-func TestKeyStabilityUpgrade(t *testing.T) {
-	t.Parallel()
-
-	// Setup options using the established pattern
-	options := &testhelper.TestOptions{
-		Testing:      t,
-		TerraformDir: "./key-stability-test",
-		Prefix:       "key-stability",
-	}
-
-	// Set variables for testing key stability
-	options.TerraformVars = map[string]interface{}{
-		"account_group_ids_to_assign": []string{"group-123", "group-456", "group-789"},
-		"account_ids_to_assign":       []string{"account-abc", "account-def", "account-xyz"},
-		"prefix":                      options.Prefix,
-	}
-
-	// Run upgrade test using the established pattern
-	output, err := options.RunTestUpgrade()
-	if !options.UpgradeTestSkipped {
-		assert.Nil(t, err, "Key stability test should not have errored")
 		assert.NotNil(t, output, "Expected some output")
 	}
 }
