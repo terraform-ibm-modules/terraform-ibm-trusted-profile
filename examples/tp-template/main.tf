cae25496219fe852253ff674965577f9257d45a5
Mukul Palit <mukul.palit@ibm.com>
2026-05-12 15:05:45 +0530

    chore(deps): update terraform terraform-ibm-modules/cos/ibm to v10.16.0 (#291)
    * chore(deps): update terraform terraform-ibm-modules/cos/ibm to v10.16.0
    
    * Fix trivy scans error and tf validation
    
    ---------
    
    Co-authored-by: Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>

--- examples/tp-template/main.tf
+++ examples/tp-template/main.tf
@@ -16,10 +16,10 @@ module "resource_group" {
 
 module "cos" {
   source            = "terraform-ibm-modules/cos/ibm"
-  version           = "10.15.2"
+  version           = "10.16.0"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${var.prefix}-cos"
-  cos_tags          = var.resource_tags
+  resource_tags     = var.resource_tags
   create_cos_bucket = false
 }
 
